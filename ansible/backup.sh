63fbc370b1b96b333f281b68e7a9d9e3c81195b9
Boyan Soubachov <bsoubachov@atlassian.com>
2020-08-12 22:04:05 +1000

    Automated backup/restore initial version

--- ansible/backup.sh
+++ ansible/backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 mkdir -p export
 
-required_applications="ansible mas"
+required_applications="mas"
 
 function requires {
     if ! command -v brew &> /dev/null
@@ -9,38 +10,57 @@ function requires {
         echo "Installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
-    echo "Installing required applications..."
-    brew install $required_applications
+    echo "Installing required applications [$required_applications]..."
+    brew install -q $required_applications
 }
 
 function clean_up {
+    echo "Cleaning up supporting software [$required_applications]..."
     brew remove $required_applications
 }
 
-function export_machine_settings {us
-    echo "exporting settings..."
+function export_machine_settings {
+    echo "* System settings..."
     mkdir -p export/defaults
 
     defaults export -globalDomain export/defaults/globalDomain.plist
 
-    for i in `defaults domains | tr ',' '\n'`
+    for domain in `defaults domains | tr ',' '\n'`
     do
-        defaults export $i export/defaults/$i.plist
+        defaults export $domain export/defaults/$domain.plist
     done
 }
 
 function export_application_settings {
+    echo "* Application settings..."
     rsync -a --prune-empty-dirs --include '*/' --include '*.plist' --exclude '*' ~/Library export/
 }
 
 function export_app_store_apps {
+    echo "* App Store applications..."
     mkdir -p export/app_store
     mas list | cut -f 1 -d ' ' > export/app_store/installed_app_ids.txt
 }
 
+function export_brew_installs {
+    echo "* Homebrew installs..."
+    mkdir -p export/homebrew
+    brew tap > export/homebrew/taps.txt
+    brew list > export/homebrew/formulae.txt
+    brew list --cask > export/homebrew/casks.txt
+}
+
+function archive_exports {
+    zip -r -q export.zip export/
+}
+
 requires
+echo "Exporting:"
 export_machine_settings
 export_application_settings
 export_app_store_apps
+export_brew_installs
+archive_exports
+
 
 echo "Done!"
